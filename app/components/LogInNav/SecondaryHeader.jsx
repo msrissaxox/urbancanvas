1f12d177a6475ecd3e3cca56c01b6017c330116a
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-05-26 18:36:21 -0400

    add style changes to the secondary header to match the primary header

--- app/components/LogInNav/SecondaryHeader.jsx
+++ app/components/LogInNav/SecondaryHeader.jsx
@@ -9,22 +9,27 @@ export default function Header() {
       const { data: session } = useSession(); //This gets the session data from next-auth
 
   return (
-    <div className="flex items-center flex-shrink-0 text-gray-300 mr-6">
-      <span className="font-semibold text-2xl md:text-3xl tracking-tight">
-        <span className="alumniSansPinstripe">Urban Mural - Street Art, Socially Mapped</span>
+    <nav className="flex items-center justify-between bg-gradient-to-tl from-stone-900 to-stone-600 p-6">
+        <div className="flex items-center flex-shrink-0 text-gray-300 mr-6">
+            <span className="font-semibold text-2xl md:text-3xl tracking-tight">
+                <span className="alumniSansPinstripe">
+                    Urban Mural - Street Art, Socially Mapped
+                    </span>
+            </span>
+        </div>
             <div className="flex flex-col items-center lg:flex-row lg:space-x-4 w-full lg:w-auto">
+
     {session ? (  
       <>
+
     <WelcomeUser />
     <HomeBtn />
     </>
     ) : (
-      <>
+
     <LoginButton />
-        </>
-    )}      
-            </div>
-        </span>
+    )}
     </div>
+    </nav>
   );
 }
