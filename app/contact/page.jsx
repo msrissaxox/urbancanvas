747d22ad0b33c0fab6bec299b62e4e6472166c29
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-06-01 00:36:47 -0400

    add header and footer to contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,8 +2,8 @@
 "use client";
 import React from "react";
 import { useForm } from "react-hook-form";
-import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
-import { ChevronDownIcon } from "@heroicons/react/16/solid";
+import SecondaryHeader from "app/components/LogInNav/SecondaryHeader";
+import Footer from "app/components/Footer/Footer";
 
 export default function Contact() {
   const {
@@ -22,7 +22,11 @@ export default function Contact() {
   const onSubmit = (data) => console.log(data);
 
   return (
-    <div className="bg-gradient-to-tl from-stone-900 to-stone-600 py-12 px-4 sm:px-6 lg:px-8">
+    <div>
+    <SecondaryHeader />
+    <h1 className="text-6xl alumniSansPinstripe font-bold text-stone-100 text-center">Contact Urban Mural</h1>
+
+    <div className="bg-gradient-to-tl from-stone-900 to-stone-800 h-full py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md mx-auto bg-stone-100 shadow-md rounded-lg p-6">
         <h2 className="text-2xl font-semibold text-gray-900 mb-6">
           Let's Connect
@@ -40,7 +44,7 @@ export default function Contact() {
                   required: "First name is required",
                 })}
                 id="firstName"
-                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm h-7"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-stone-500 focus:ring-stone-500 sm:text-sm h-7"
               />
               {errors.firstName && (
                 <span className="text-red-500 text-sm">
@@ -60,7 +64,7 @@ export default function Contact() {
                 type="text"
                 {...register("lastName")}
                 id="lastName"
-                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm h-7"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-stone-500 focus:ring-stone-500 sm:text-sm h-7"
               />
             </label>
           </div>
@@ -75,7 +79,7 @@ export default function Contact() {
                 type="email"
                 {...register("email", { required: "Email is required" })}
                 id="email"
-                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm h-7"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-stone-500 focus:ring-stone-500 sm:text-sm h-7"
               />
               {errors.email && (
                 <span className="text-red-500 text-sm">
@@ -101,7 +105,7 @@ export default function Contact() {
                 })}
                 id="message"
                 rows={4}
-                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-stone-500 focus:ring-stone-500 sm:text-sm"
               />
               {errors.text && (
                 <span className="text-red-500 text-sm">
@@ -121,7 +125,7 @@ export default function Contact() {
                 type="url"
                 {...register("linkedIn")}
                 id="linkedin"
-                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm h-7"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-stone-500 focus:ring-stone-500 sm:text-sm h-7"
               />
             </label>
           </div>
@@ -129,13 +133,15 @@ export default function Contact() {
           <div>
             <button
               type="submit"
-              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-stone-600 hover:bg-stone-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-stone-500"
             >
               Submit
             </button>
           </div>
         </form>
       </div>
+    </div>
+    <Footer />
     </div>
   );
 }
