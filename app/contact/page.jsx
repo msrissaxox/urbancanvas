c9ab2c232ae32bb0ea8673d992f57db2cecf1e93
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-06-01 00:20:32 -0400

    add styling to form

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,8 +2,8 @@
 "use client";
 import React from "react";
 import { useForm } from "react-hook-form";
-import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid';
-import { ChevronDownIcon } from '@heroicons/react/16/solid';
+import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+import { ChevronDownIcon } from "@heroicons/react/16/solid";
 
 export default function Contact() {
   const {
@@ -22,69 +22,106 @@ export default function Contact() {
   const onSubmit = (data) => console.log(data);
 
   return (
-    <div className="bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md mx-auto bg-white shadow-md rounded-lg p-6">
-        <h2 className="text-2xl font-semibold text-gray-900 mb-6">Contact Us</h2>
+    <div className="bg-gradient-to-tl from-stone-900 to-stone-600 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md mx-auto bg-stone-100 shadow-md rounded-lg p-6">
+        <h2 className="text-2xl font-semibold text-gray-900 mb-6">
+          Let's Connect
+        </h2>
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
           <div>
-            <label htmlFor="firstName" className="block text-sm font-medium text-gray-700">
+            <label
+              htmlFor="firstName"
+              className="block text-sm font-medium text-gray-700"
+            >
               First Name:
               <input
                 type="text"
-                {...register("firstName", { required: "First name is required" })}
+                {...register("firstName", {
+                  required: "First name is required",
+                })}
                 id="firstName"
-                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm h-7"
               />
-              {errors.firstName && <span className="text-red-500 text-sm">{errors.firstName.message}</span>}
+              {errors.firstName && (
+                <span className="text-red-500 text-sm">
+                  {errors.firstName.message}
+                </span>
+              )}
             </label>
           </div>
 
           <div>
-            <label htmlFor="lastName" className="block text-sm font-medium text-gray-700">
+            <label
+              htmlFor="lastName"
+              className="block text-sm font-medium text-gray-700"
+            >
               Last Name:
               <input
                 type="text"
                 {...register("lastName")}
                 id="lastName"
-                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm h-7"
               />
             </label>
           </div>
 
           <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
               Email:
               <input
                 type="email"
                 {...register("email", { required: "Email is required" })}
                 id="email"
-                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm h-7"
               />
-              {errors.email && <span className="text-red-500 text-sm">{errors.email.message}</span>}
+              {errors.email && (
+                <span className="text-red-500 text-sm">
+                  {errors.email.message}
+                </span>
+              )}
             </label>
           </div>
 
           <div>
-            <label htmlFor="message" className="block text-sm font-medium text-gray-700">
+            <label
+              htmlFor="message"
+              className="block text-sm font-medium text-gray-700"
+            >
               Message:
               <textarea
-                {...register("text", { required: "Please enter your message" })}
+                {...register("text", {
+                  required: "Please enter your message",
+                  minLength: {
+                    value: 20,
+                    message: "Message must be at least 20 characters long",
+                  },
+                })}
                 id="message"
                 rows={4}
                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
               />
-              {errors.text && <span className="text-red-500 text-sm">{errors.text.message}</span>}
+              {errors.text && (
+                <span className="text-red-500 text-sm">
+                  {errors.text.message}
+                </span>
+              )}
             </label>
           </div>
 
           <div>
-            <label htmlFor="linkedin" className="block text-sm font-medium text-gray-700">
+            <label
+              htmlFor="linkedin"
+              className="block text-sm font-medium text-gray-700"
+            >
               LinkedIn:
               <input
                 type="url"
                 {...register("linkedIn")}
                 id="linkedin"
-                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm h-7"
               />
             </label>
           </div>
