ff0a154689cf90528c94139e88826b6fb86257f1
msrissaxox <marissa.bianca.lamothe@gmail.com>
2025-06-27 18:51:05 -0400

    update middleware

--- middleware.js
+++ middleware.js
@@ -7,12 +7,12 @@ export function middleware(request) {
     "Content-Security-Policy",
     [
       "default-src 'self';",
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://accounts.google.com https://apis.google.com https://va.vercel-scripts.com;",
+      "script-src 'self' https://www.googletagmanager.com https://accounts.google.com https://apis.google.com https://va.vercel-scripts.com;",
       "style-src 'self' 'unsafe-inline' fonts.googleapis.com;",
       "img-src 'self' data: https:;",
       "font-src 'self' fonts.gstatic.com;",
-      "connect-src 'self' https://*.supabase.co https://urbancanvas.vercel.app https://*.vercel.app https://accounts.google.com https://apis.google.com;",
-      "frame-src https://accounts.google.com;",
+      "connect-src 'self' https://*.supabase.co https://urbancanvas.vercel.app https://*.vercel.app https://accounts.google.com https://apis.google.com https://va.vercel-scripts.com;",
+      "frame-src 'self' https://accounts.google.com;",
       "object-src 'none';",
       "base-uri 'self';",
       "form-action 'self';"
